e3f17aeef2fcdbef175c7add25cc92845c32a228
Bizzare <tech.sanket29@gmail.com>
2022-07-29 14:14:25 +0530

    sign up done

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,22 +3,68 @@
 <h2>Sign Up</h2>
 
 <div class="register">
-    <input type="text" placeholder="Enter Name"/>
-    <input type="email" placeholder="Enter Email"/>
-    <input type="password" placeholder="Enter Password"/>
-    <button type="button">Sign Up</button>
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="email" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signup()" type="button">Sign Up</button>
 </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
     name: "SignUp",
+    data() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+        };
+    },
+
+    methods: {
+        async signup() {
+            console.warn("clicked", this.name, this.email, this.password);
+            let result = await axios.post("http://localhost:3000/users", {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+            });
+            console.warn(result);
+            if (result.status == 201) {
+                alert("Sign Up Done");
+            }
+            localStorage.setItem("user-info",JSON.stringify(result.data))
+        },
+    },
 };
 </script>
 
 <style scoped>
-.logo{width: 150px;}
-.register input{width: 300px; height: 40px; padding-left: 20px; display: block; margin: 0 auto 30px auto; border: 1px solid #c7c7c7; }
-.register button{width: 320px; height: 40px; border: 1px solid #f29400; color: #ffffff; background-color:#f29400; cursor: pointer;}
-.register button:hover{background-color: transparent; color: #f29400;}
+.logo {
+    width: 150px;
+}
+
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin: 0 auto 30px auto;
+    border: 1px solid #c7c7c7;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid #f29400;
+    color: #ffffff;
+    background-color: #f29400;
+    cursor: pointer;
+}
+
+.register button:hover {
+    background-color: transparent;
+    color: #f29400;
+}
 </style>
