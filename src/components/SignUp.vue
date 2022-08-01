777a963cecac38d28d30a5af301228f02c81fb09
Bizzare <tech.sanket29@gmail.com>
2022-08-01 14:43:25 +0530

    changes discarded

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,70 +1,70 @@
 <template>
-  <img class="logo" src="../assets/restro-logo.png" />
-  <h2>Sign Up</h2>
+<img class="logo" src="../assets/restro-logo.png" />
+<h2>Sign Up</h2>
 
-  <div class="register">
+<div class="register">
     <input type="text" v-model="name" placeholder="Enter Name" />
     <input type="email" v-model="email" placeholder="Enter Email" />
     <input type="password" v-model="password" placeholder="Enter Password" />
     <button v-on:click="signup()" type="button">Sign Up</button>
-  </div>
+</div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: "SignUp",
-  data() {
-    return {
-      name: "",
-      email: "",
-      password: "",
-    };
-  },
-
-  methods: {
-    async signup() {
-      console.warn("clicked", this.name, this.email, this.password);
-      let result = await axios.post("http://localhost:3000/users", {
-        name: this.name,
-        email: this.email,
-        password: this.password,
-      });
-      console.warn(result);
-      if (result.status == 201) {
-        alert("Sign Up Done");
-      }
-      localStorage.setItem("user-info", JSON.stringify(result.data));
+    name: "SignUp",
+    data() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+        };
     },
-  },
+    
+    methods: {
+        async signup() {
+            console.warn("clicked", this.name, this.email, this.password);
+            let result = await axios.post("http://localhost:3000/users", {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+            });
+            console.warn(result);
+            if (result.status == 201) {
+                alert("Sign Up Done");
+            }
+            localStorage.setItem("user-info", JSON.stringify(result.data));
+        },
+    },
 };
 </script>
 
 <style scoped>
 .logo {
-  width: 150px;
+    width: 150px;
 }
 
 .register input {
-  width: 300px;
-  height: 40px;
-  padding-left: 20px;
-  display: block;
-  margin: 0 auto 30px auto;
-  border: 1px solid #c7c7c7;
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin: 0 auto 30px auto;
+    border: 1px solid #c7c7c7;
 }
 
 .register button {
-  width: 320px;
-  height: 40px;
-  border: 1px solid #f29400;
-  color: #ffffff;
-  background-color: #f29400;
-  cursor: pointer;
+    width: 320px;
+    height: 40px;
+    border: 1px solid #f29400;
+    color: #ffffff;
+    background-color: #f29400;
+    cursor: pointer;
 }
 
 .register button:hover {
-  background-color: transparent;
-  color: #f29400;
+    background-color: transparent;
+    color: #f29400;
 }
 </style>
