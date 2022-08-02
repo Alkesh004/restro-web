b0a37f040ae2fa9a76597483dc6abb14d70db2da
Bizzare <tech.sanket29@gmail.com>
2022-08-02 12:12:00 +0530

    modified login component

--- src/components/LoginCompo.vue
+++ src/components/LoginCompo.vue
@@ -6,7 +6,7 @@
 
     <input type="email" v-model="email" placeholder="Enter Email" />
     <input type="password" v-model="password" placeholder="Enter Password" />
-    <button type="button">Login</button>
+    <button v-on:click="login" type="button">Login</button>
     <p>
         Don't have an account ? <router-link to="/sign-up">Sign Up</router-link>
     </p>
@@ -14,8 +14,26 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'LoginCompo',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+
+    methods: {
+        async login() {
+            let result = await axios.get(
+                `http://localhost:3000/users?email=${this.email}&password=${this.paaword}`
+            );
+            console.warn(result);
+        }
+    },
+
     mounted() {
         let user = localStorage.getItem('user-info');
         if (user) {
@@ -25,5 +43,4 @@ export default {
         }
     }
 }
-
 </script>
